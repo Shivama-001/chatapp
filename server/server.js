029fa0b0ac0d0c6a558f88c640b6823c9e203d82
shivam kumar <hjujhytuytutyurytu>
2026-03-15 12:11:52 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -65,8 +65,12 @@ app.use("/api/messages", messageRouter);
 await connectDB();
 
 // Start server
+
+if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
 });
+
+}
