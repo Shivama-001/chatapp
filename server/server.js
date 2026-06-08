f6d2cdb825ec1afbeb49b12df0272f616db92629
shivam kumar <hjujhytuytutyurytu>
2026-06-08 20:04:28 +0530

    server fix

--- server/server.js
+++ server/server.js
@@ -55,8 +55,7 @@ app.use(express.json({ limit: "4mb" }));
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    process.env.CLIENT_URL
+    "http://localhost:5173"
   ],
   credentials: true
 }));
